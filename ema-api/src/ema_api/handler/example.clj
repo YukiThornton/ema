15eb3ab52d97061d5f38bf5fa96d5ed6af73208e
YukiThornton <yukiokuno16@gmail.com>
2019-05-11 19:14:18 +0900

    APIs return fake responses

--- ema-api/src/ema_api/handler/example.clj
+++ ema-api/src/ema_api/handler/example.clj
@@ -3,10 +3,27 @@
             [ataraxy.response :as response] 
             [integrant.core :as ig]))
 
-(defmethod ig/init-key :ema-api.handler/example [_ options]
-  (fn [{[_] :ataraxy/result}]
-    [::response/ok {:example "data"}]))
+(def sample-emas
+  {:emas
+   [{:id 1
+     :user-id 1
+     :type "wish"
+     :status "open"
+     :content
+     {:text "Cras elementum at nulla sit amet lobortis. Praesent diam ligula, ultrices sed rutrum eu, iaculis id turpis. Phasellus interdum gravida odio, ut dapibus ante sodales quis. Nam vel suscipit leo. In odio quam, placerat sit amet lectus non, aliquam dignissim lectus. Vestibulum vehicula, magna bibendum consectetur ullamcorper, nibh ligula convallis dolor"}}
+    {:id 2
+     :user-id 1
+     :type "wish"
+     :status "open"
+     :content
+     {:text "Cras elementum tortor eu elementum commodo. Nam eget tellus purus. Donec id sem feugiat, sodales elit eget, rhoncus ipsum. Donec varius neque eu suscipit lacinia. Proin et lorem vehicula, imperdiet urna a, finibus dui."}}]})
 
 (defmethod ig/init-key :ema-api.handler/emas [_ options]
   (fn [{[_] :ataraxy/result}]
-    [::response/ok {:emas "emas!"}]))
+    [::response/ok sample-emas]))
+
+
+(defmethod ig/init-key :ema-api.handler/create-ema [_ options]
+  (fn [{[_ body] :ataraxy/result}]
+    [::response/created 1 {:result "ok"
+                           :ema (body :params)}]))
