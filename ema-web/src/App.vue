4c61984d1728d4cbb1823b5b38bfef55ead6fded
YukiThornton <yukiokuno16@gmail.com>
2019-06-03 09:40:18 +0900

    Add Docker and kubernetes config for ema-web

--- ema-web/src/App.vue
+++ ema-web/src/App.vue
@@ -9,6 +9,11 @@
 </template>
 
 <script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class App extends Vue {}
+
 </script>
 
 <style lang="scss">
